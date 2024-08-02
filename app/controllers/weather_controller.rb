ca370dbe12ebe257ff30730bc62dceaed68af437
JeffDev17 <jeeff.araujo1@gmail.com>
2024-08-02 16:09:52 -0300

    search bar fully working and registering pokes

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,24 +1,12 @@
 class WeatherController < ApplicationController
   def index
-    @city = params[:city]
-    if @city.present?
-      weather_data = get_weather_for(@city)
-      @temperature = weather_data[:temperature]
-      @pokemon = select_pokemon_based_on_weather(@temperature)
-    end
   end
 
-  def search(city)
-    # Replace this with actual API call and processing logic
-    Search.new.run
+  def search
+    city = params[:city].capitalize
+    Search.new.run(city)
+    render :index
   end
 
-  private
 
-  
-
-  def select_pokemon_based_on_weather(temperature)
-    # Logic to select a Pokémon based on temperature
-    "Pikachu" # Example Pokémon
-  end
 end
