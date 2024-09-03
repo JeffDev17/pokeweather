db915c4cb0c6af6bcb8e34d4a1dd65ccc921facc
JeffDev17 <jeeff.araujo1@gmail.com>
2024-09-03 17:00:11 -0300

    Pokemon sprites added

--- app/services/search.rb
+++ app/services/search.rb
@@ -1,37 +1,41 @@
 class Search 
 
-    def run(city)
-        data = consult_weather(city)
-        temperature = data[0]
-        rain = data[1]
-        city = data[2]
-        type = check_type(temperature, rain).downcase
+  def run(city)
+    data = consult_weather(city)
+    temperature = data[0]
+    rain = data[1]
+    city = data[2]
+    type = check_type(temperature, rain).downcase
   
-        pokemon_selecionado = consult_pokemon(type)
-        pokemon_selecionado.capitalize
+    pokemon_selecionado = PokemonApi.new.run(type)
+    pokemon_selecionado.capitalize!
+  
+    # Register in SearchHistory table
+    register(city, temperature, pokemon_selecionado)
+  
+    # Build the Pokémon object (name, height, weight, dexNo, type)
+    object, types = build_pokemon_object(pokemon_selecionado)
+  
+    # Register or find the Pokémon
+    pokemon = register_dex(object)
+  
+    # Create Pokémon types
+    create_pokemon_type(pokemon, types)
+  
+    # Attach the sprite image
+    attach_sprite_image(pokemon, object[:sprite_image])
+  
+    # Return data for display
+    weather_data = {
+      pokemon: pokemon_selecionado,
+      temperature: temperature,
+      rain: rain,
+      city: city
+    }
+    puts weather_data.inspect
 
-        #regitra na tabela SearchHistory 
-        register(city, temperature, pokemon_selecionado.capitalize)
-        
-        #Monta o objeto pokemon (nome, altura, peso, dexNo, tipo)
-        object = build_pokemon_object(pokemon_selecionado)
-
-        
-        pokemon = register_dex(object.first)
-
-        create_pokemon_type(pokemon, object.last)
-
-        #retornando valor p dar display no notice
-        weather_data = {
-          pokemon: pokemon_selecionado.capitalize,
-          temperature: temperature,
-          rain: rain,
-          city: city
-        }
-        
-        weather_data
-
-    end
+    weather_data
+  end
 
     private  
 
@@ -74,26 +78,37 @@ class Search
     end
 
     def register_dex(object)
-      #Vai buscar(find) o (object: name) 
-      #na tabela de Pokemon
-      #e se não encontrar na lista vai criar 
-      #um pokemon usando os dados do Hash (object) 
-      Pokemon.create_with(object).find_or_create_by(name: object[:name])
+      pokemon = Pokemon.find_or_create_by(name: object[:name])
+    
+      # organiza os attributos (remover????)
+      pokemon.dex = object[:dex]
+      pokemon.height = object[:height]
+      pokemon.weight = object[:weight]
+    
+      # Cria o pokemon
+      pokemon.save!
+    
+      # attach sprite separadamente
+      if object[:sprite_image].present?
+        attach_sprite_image(pokemon, object[:sprite_image])
+      end
+    
+      pokemon
     end
 
+
     def build_pokemon_object(pokemon_selecionado)
-      #Roda a API
-      #Monta o pokemon retornando os dados num hash
       pokemon_data = PokemonApi.new.register(pokemon_selecionado)
-     
-      pokemon_object = {
+    
+      object = {
         name: pokemon_data['name'].capitalize,
         dex: pokemon_data['id'],
         height: pokemon_data['height'],
-        weight: pokemon_data['weight']
+        weight: pokemon_data['weight'],
+        sprite_image: pokemon_data.dig('sprites', 'other', 'official-artwork', 'front_default')
       }
-
-      [pokemon_object, pokemon_data['types']]
+    
+      [object, pokemon_data['types']]
     end
 
     def create_pokemon_type(pokemon, types)
@@ -101,6 +116,16 @@ class Search
       type_seed = Type.find_by(name: type['type']['name'].capitalize)
       PokemonType.create!(pokemon: pokemon, type: type_seed)
       end
+    end
+
+    def attach_sprite_image(pokemon, image_url)
+      return unless image_url.present?
+
+      image_data = RestClient.get(image_url)
+      image_file = StringIO.new(image_data.body)
+      
+      pokemon.sprite_image.attach(io: image_file, filename: "#{pokemon.name.downcase}.png")
+      pokemon.save
     end
 
 end
